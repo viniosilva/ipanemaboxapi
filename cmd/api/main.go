003a8ee5e02376445ee9ba2067f8bc77ce1bf541
viniosilva <oliveira.vinicius.92@gmail.com>
2024-10-11 07:43:16 -0300

    add cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -55,6 +55,7 @@ func main() {
 		WithSpanID:  true,
 		WithTraceID: true,
 	}))
+	router.Use(CORSMiddleware())
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET("/api/healthcheck", factory.HealthCheckController.Check)
@@ -82,4 +83,20 @@ func main() {
 	}
 
 	slog.Info("server exiting")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
